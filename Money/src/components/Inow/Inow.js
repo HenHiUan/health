6444054f39f64304e588d27758e458d62e8706e2
HenHiUan <1339234541@qq.com>
2019-06-27 22:41:46 +0800

    del

--- Money/src/components/Inow/Inow.js
+++
@@ -1,9 +0,0 @@
-import React,{Component} from 'react';
-
-export default class Inow extends Component{
-    render(){
-        return(
-            <div className="buttons" id="buttons"><p className="p1">马上加入</p></div>
-        )
-    }
-}
--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # health
 
 [健康有益](https://henhiuan.github.io/health/Health/健康有益.html)
-
-[悟空理财](https://henhiuan.github.io/health/Money/public/index.html)
